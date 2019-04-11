bd35de9205bd8f0cd105d8f11151cb861cc6b2e3
Kelsey Low <kklowlow@gmail.com>
2019-04-11 16:42:59 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_character.rb
+++ db/migrate/006_add_catchphrase_to_character.rb
@@ -1,8 +1,5 @@
-class CreateCharacters < ActiveRecord::Migration[4.2]
+class AddCatchphraseToCharacter < ActiveRecord::Migration[4.2]
   def change
-    create_table :characters do |t|
-      t.string :name
-      t.integer :show_id
-    end
+    
   end
 end
